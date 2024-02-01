5ba30aa40d7266a520e738958ad6903b32bdd54e
HungHiro <tuanhung.bka@gmail.com>
2024-02-02 03:02:52 +0700

    bvn-20

--- F8-FT-k5/BVN-no20/js/BVN-no20.js
+++ F8-FT-k5/BVN-no20/js/BVN-no20.js
@@ -3,20 +3,21 @@ let getParagraph = document.querySelector(".lorem-p");
 let strParagraph = document.querySelector(".lorem-p").innerHTML;
 let arrWords = strParagraph.split(" ");
 let arrWordsLength = arrWords.length;
-let i = 0;
+let a = 0;
 
 function setColorParagraph() {
-    let arrContent = arrWords.map((word, a) => {
-        if (a === i) {
-            return `<span class="highlight">${word}</span>`
-        } else {
-            return `<span class="default">${word}</span>`
-        }
+    let arrContent = arrWords.map((word, index) => {
+        return index === a ? `<span class="highlight">${word}</span>` : `<span class="default">${word}</span>`;
+        // if (index === a) {
+        //     return `<span class="highlight">${word}</span>`
+        // } else {
+        //     return `<span class="default">${word}</span>`
+        // }
     }).join(" ");
     getParagraph.innerHTML = arrContent;
-    i++;
-    if (i >= arrWordsLength) {
-        i = 0;
+    a++;
+    if (a >= arrWordsLength) {
+        a = 0;
     }
 }
 setInterval(setColorParagraph, 1000);
